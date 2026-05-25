e0ecafaf34f3afc1cc7c3326239fc804f29b7fd8
Nikita Permyakov <permakovnikita70@gmail.com>
2026-05-25 22:08:27 +0800

    Added requirements and service

---
+++ src/service.py
@@ -0,0 +1,17 @@
+import requests
+import numpy as np
+import fastapi
+
+app = fastapi.FastAPI()
+
+def fetch_text(url: str) -> str:
+    """Получение текста из URL-адреса."""
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+
+def frequency_analysis(text: str) -> dict[str, int]:
+    """Подсчёт частоты встречаемости слов с помощью библиотеки numpy"""
+    words = text.lower().split()
+    unique, counts = np.unique(words, return_counts=True)
+    return dict(zip(unique, counts))
